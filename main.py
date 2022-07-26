7ea02109c8fb5688ef06eb5232c0d30f8833a9f9
Martin <martinpellizzer@gmail.com>
2022-07-26 12:29:30 +0200

    _

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from time import sleep
 
 import re
+import os
 import requests
 from urllib.parse import urlsplit
 from collections import deque
@@ -11,18 +12,11 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 driver = webdriver.Chrome('./chromedriver')
-
+driver.maximize_window()
 driver.get('https://www.google.com')
-sleep(3)
+sleep(2)
 driver.find_element(By.XPATH, '//div[text()="Rifiuta tutto"]').click()
-
-sleep(3)
-driver.get('https://www.google.com/maps/search/cantine+near+me')
-#driver.get('https://www.google.com/search?q=cantine+near+me')
-
-elements = driver.find_elements(By.XPATH, '//div[@role="article"]')
-print(len(elements))
-print()
+sleep(2)
 
 def scrape_emails(url):
 	parts = urlsplit(url)
@@ -30,10 +24,6 @@ def scrape_emails(url):
 
 	try: response = requests.get(base_url)
 	except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError): return ''
-
-	# return re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', response.text)
-	
-	# print(re.findall(r'[a-zA-Z.]+@[a-zA-Z]+\.(com)', response.text)
 
 	matches = re.finditer(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.(com|net|edu|it|eu)', response.text)
 
@@ -43,11 +33,57 @@ def scrape_emails(url):
 
 	return emails
 
-websites = []
-for i, e in enumerate(elements):
-	if i > 10: break
-	e.click()
-	sleep(5)
+def get_new_business():
+	todo = ''
+	elements = driver.find_elements(By.XPATH, '//div[@role="article"]')
+
+	for e in elements:
+		id = e.get_attribute('aria-label')
+		if id not in done:
+			done.append(id)
+			todo = e
+			break
+
+	return todo
+	
+def scroll_down_up_down():
+	driver.find_element(By.XPATH, '//div[@role="feed"]').send_keys(Keys.PAGE_DOWN)
+	sleep(2)
+	driver.find_element(By.XPATH, '//div[@role="feed"]').send_keys(Keys.PAGE_UP)
+	sleep(2)
+	driver.find_element(By.XPATH, '//div[@role="feed"]').send_keys(Keys.PAGE_DOWN)
+	sleep(2)
+	
+
+
+try: os.remove('document.csv')
+except OSError: pass
+
+driver.get('https://www.google.com/maps/search/azienda+vitivinicola+friuli')
+#driver.get('https://www.google.com/search?q=cantine+near+me')
+
+done = []
+for i in range(20):
+	sleep(2)
+
+	# get the first new business found
+	todo = get_new_business()
+
+	# if not new business found, try to load more businesses
+	if not todo:
+		print('nothing new...')
+		scroll_down_up_down()
+		continue
+	
+	aria_label = todo.get_attribute('aria-label')
+	print(aria_label)
+
+	try: todo.click()
+	except:
+		scroll_down_up_down()
+		try: todo.click()
+		except: continue
+	sleep(2)
 
 	name = ''
 	phone = ''
@@ -55,9 +91,13 @@ for i, e in enumerate(elements):
 	website = ''
 	emails = []
 
-	element_main = e.find_elements(By.XPATH, '//div[@role="main"]')[1]
+	try: element_main = todo.find_elements(By.XPATH, '//div[@role="main"]')[1]
+	except: continue
+
 	try: name = element_main.find_element(By.XPATH, './/h1').text
 	except: pass
+	if name != aria_label: continue
+
 	try: address = element_main.find_element(By.XPATH, './/button[@data-item-id="address"]').text
 	except: pass
 	try: website = element_main.find_element(By.XPATH, './/a[@data-item-id="authority"]').get_attribute("href")
@@ -72,7 +112,11 @@ for i, e in enumerate(elements):
 	print(website)
 	print(phone)
 	print(emails)
-
 	print()
 
-	# driver.find_element(By.XPATH, '//div[@role="feed"]').send_keys(Keys.PAGE_DOWN)
+	s_emails =' '.join(emails)
+
+	divider = ';'
+
+	with open('document.csv','a') as f:
+		f.write(f'{name}{divider}{address}{divider}{website}{divider}{phone}{divider}{s_emails}\n')
