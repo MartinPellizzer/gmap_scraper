44bdedeceeb944d729e62012ed198e0becf78a10
Martin Pellizzer <martinpellizzer@gmail.com>
2022-08-28 19:07:54 +0200

    refactor

--- main.py
+++ main.py
@@ -9,14 +9,57 @@ import requests
 from urllib.parse import urlsplit
 from collections import deque
 from bs4 import BeautifulSoup
-import pandas as pd
 
 import sys
 import csv
 
 driver = None
-num_operations = 5
 
+num_operations = 5
+sep = ';'
+
+
+######################################################################################
+# CSV
+######################################################################################
+def create_csv():
+	with open('document.csv', 'w', encoding='utf-8') as f:
+		f.write(f'name{sep}address{sep}website{sep}phone{sep}emails\n')
+
+def get_old_businesses():
+	global sep
+	if os.path.isfile('document.csv'):
+		names = list()
+		with open('document.csv', 'r', encoding="utf-8") as f:
+			lines = f.readlines()
+			for line in lines:
+				names.append(line.split(sep)[0])
+		return names
+	else: 
+		return []
+		
+def get_old_businesses_pandas():
+	global sep
+	if os.path.isfile('document.csv'):
+		df = pandas.read_csv('document.csv', sep=sep)
+		return df['name'].to_list()
+	else: 
+		create_csv()
+		return []
+
+
+######################################################################################
+# STRINGS
+######################################################################################
+def to_ascii(text):
+	encoded_string = text.encode('ascii', 'ignore')
+	decoded_string = encoded_string.decode()
+	return text
+
+
+######################################################################################
+# EMAILS
+######################################################################################
 def find_contact_url(url):
 	contact_page = ''
 
@@ -27,12 +70,10 @@ def find_contact_url(url):
 		if 'contatti' in str(link).lower():
 			link = link.get('href')
 			contact_page = link
-			print(link)
+			# print(link)
 	
 	return contact_page
 
-def scrape_validate_emails():
-	pass
 
 def scrape_emails(url):
 	emails = set()
@@ -49,13 +90,13 @@ def scrape_emails(url):
 		response = requests.get(url)
 		matches = re.finditer(regex_string, response.text)
 		for match in matches:
-			print(parts.netloc + ' --> ' + match.group())
+			# print(parts.netloc + ' --> ' + match.group())
 			for domain in list_domains:
 				if domain in match.group():
 					emails.add(match.group())
 					break
 	except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError): 
-		print('*** cant load page')
+		# print('*** cant load page')
 		return set()
 
 	# contact page 1
@@ -64,73 +105,37 @@ def scrape_emails(url):
 		response = requests.get(contact_page)
 		matches = re.finditer(regex_string, response.text)
 		for match in matches:
-			print(parts.netloc + ' --> ' + match.group())
+			# print(parts.netloc + ' --> ' + match.group())
 			for domain in list_domains:
 				if domain in match.group():
 					emails.add(match.group())
 					break
 	except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError): 
-		print('*** cant find contact link')
+		# print('*** cant find contact link')
 		pass
 		
 	# contact page 2
 	last_resort_url = f'{url}/contatti'
-	print(f'LAST RESORT: {last_resort_url}')
+	# print(f'LAST RESORT: {last_resort_url}')
 	try:
 		response = requests.get(last_resort_url)
 		matches = re.finditer(regex_string, response.text)
 		for match in matches:
-			print(parts.netloc + ' --> ' + match.group())
+			# print(parts.netloc + ' --> ' + match.group())
 			for domain in list_domains:
 				if domain in match.group():
 					emails.add(match.group())
 					break
 	except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
-		print('*** has no contact url')
+		# print('*** has no contact url')
 		pass
 		
 	return emails
 
-def get_new_business():
-	todo = ''
-	elements = driver.find_elements(By.XPATH, '//div[@role="article"]')
-
-	for e in elements:
-		id = e.get_attribute('aria-label')
-		if id not in done:
-			done.append(id)
-			todo = e
-			break
-
-	return todo
 	
-def scroll_down_up_down():
-	driver.find_element(By.XPATH, '//div[@role="feed"]').send_keys(Keys.PAGE_DOWN)
-	sleep(2)
-	driver.find_element(By.XPATH, '//div[@role="feed"]').send_keys(Keys.PAGE_UP)
-	sleep(2)
-	driver.find_element(By.XPATH, '//div[@role="feed"]').send_keys(Keys.PAGE_DOWN)
-	sleep(2)
-	
-
-
-done = []
-
-try: os.remove('document.csv')
-except OSError: pass
-
-try:
-	with open('document.csv', mode='r') as f:
-		csv_reader = csv.reader(f, delimiter=',')
-		for row in f:
-			info = row.split(';')
-			done.append(info[0])
-except:
-	pass
-
-
-
-
+######################################################################################
+# BROWSER
+######################################################################################
 def open_browser():
 	global driver
 	driver = webdriver.Chrome('./chromedriver')
@@ -139,82 +144,143 @@ def open_browser():
 	sleep(2)
 	driver.find_element(By.XPATH, '//div[text()="Rifiuta tutto"]').click()
 	sleep(2)
-open_browser()
 
 
-search_text = sys.argv[1]
-search_text = search_text.replace(' ', '+')
-print(search_text)
-driver.get(f'https://www.google.com/maps/search/{search_text}')
-#driver.get(f'https://www.google.com/maps/search/azienda+vinicola+padova')
-
-for i in range(num_operations):
-	print(f'SCRAPING: {i}/{num_operations}')
+def scroll_down_up_down():
+	global driver
+	driver.find_element(By.XPATH, '//div[@role="feed"]').send_keys(Keys.PAGE_DOWN)
+	sleep(2)
+	driver.find_element(By.XPATH, '//div[@role="feed"]').send_keys(Keys.PAGE_UP)
+	sleep(2)
+	driver.find_element(By.XPATH, '//div[@role="feed"]').send_keys(Keys.PAGE_DOWN)
 	sleep(2)
 
-	# get the first new business found
-	todo = get_new_business()
 
-	# if not new business found, try to load more businesses
-	if not todo:
-		print('---------------------------------------------')
-		print('nothing new...')
-		print('---------------------------------------------')
-		print()
-		scroll_down_up_down()
-		continue
+def search_map():
+	driver.get(f'https://www.google.com/maps/search/azienda+vinicola+padova')
+
+
+def click_on_listing(business):
+	for _ in range(3):
+		try: 
+			business.click()
+			return True
+		except: 
+			continue
+	return False
+
+
+######################################################################################
+# SCRAPE
+######################################################################################
+def get_card_element(e):
+	try: return e.find_elements(By.XPATH, '//div[@role="main"]')[1]
+	except: return None
 	
-	aria_label = todo.get_attribute('aria-label')
-	print(aria_label)
 
-	try: todo.click()
-	except:
-		scroll_down_up_down()
-		try: todo.click()
-		except: continue
-	sleep(2)
+def scrape_name(e):
+	try: return e.find_element(By.XPATH, './/h1').text
+	except: return ''
 
-	name = ''
-	phone = ''
-	address = ''
-	website = ''
-	emails = []
 
-	try: element_main = todo.find_elements(By.XPATH, '//div[@role="main"]')[1]
-	except: continue
+def scrape_address(e):
+	try: return e.find_element(By.XPATH, './/button[@data-item-id="address"]').text
+	except: return ''
 
-	try: name = element_main.find_element(By.XPATH, './/h1').text
-	except: pass
-	
-	if name != aria_label: continue
 
-	try: address = element_main.find_element(By.XPATH, './/button[@data-item-id="address"]').text
-	except: pass
+def scrape_website(e):
 	try: 
-		website = element_main.find_element(By.XPATH, './/a[@data-item-id="authority"]').get_attribute("href")
+		website = e.find_element(By.XPATH, './/a[@data-item-id="authority"]').get_attribute("href")
 		parts = urlsplit(website)
 		website = f"{parts.scheme}://{parts.netloc}"
-	except: pass
-	try: phone = element_main.find_element(By.XPATH, './/button[contains(@data-item-id, "phone")]').text
-	except: pass
+		return website
+	except: return ''
 
-	emails = scrape_emails(website)
-	
-	print(name)
-	print(address)
-	print(website)
-	print(phone)
-	print(emails)
+
+def scrape_phone(e):
+	try: return e.find_element(By.XPATH, './/button[contains(@data-item-id, "phone")]').text
+	except: return ''
+
+
+def find_new_business(old_businesses):
+	global driver
+	elements = driver.find_elements(By.XPATH, '//div[@role="article"]')
+	for e in elements:
+		name = e.get_attribute('aria-label')
+		if name not in old_businesses:
+			return e, name
+	return None, None
+
+def debug_info(name, address, website, phone, emails):
+	print(f'{"Name:":<8} {name}')
+	print(f'{"Address:":<8} {address}')
+	print(f'{"Website:":<8} {website}')
+	print(f'{"Phone:":<8} {phone}')
+	print(f'{"Emails:":<8} {emails}')
+	print(f'{"":->64}')
 	print()
 
-	s_emails =' '.join(emails)
+def scrape_new_business():
+	# get already scraped businesses
+	old_businesses = get_old_businesses_pandas()
 
-	divider = ';'
+	# get the first new business that was not previously scraped
+	business, label = find_new_business(old_businesses)
 
-	string_to_write = f'{name}{divider}{address}{divider}{website}{divider}{phone}{divider}{s_emails}\n'
-	string_to_write = string_to_write.replace('"', '')
+	# if not new businesses found, scroll down the page to load more
+	if not business:
+		scroll_down_up_down()
+		return
 
-	with open('document.csv','a', encoding="utf-8") as f:
+	# google maps is bugged: scroll a bit the screen and try clicking again if needed
+	if not click_on_listing(business):
+		return
+
+	sleep(2)
+	
+	card_element = get_card_element(business)
+
+	name = scrape_name(card_element)
+	if name != label: return
+
+	address = scrape_address(card_element)
+	website = scrape_website(card_element)
+	phone = scrape_phone(card_element)
+	emails = scrape_emails(website)
+	# s_emails = ' '.join(emails)
+
+	debug_info(name, address, website, phone, emails)
+
+	global csv_sep
+	string_to_write = ''
+	string_to_write += f'{name}{csv_sep}'
+	string_to_write += f'{address}{csv_sep}'
+	string_to_write += f'{website}{csv_sep}'
+	string_to_write += f'{phone}{csv_sep}'
+	string_to_write += f'{emails}\n'
+
+	with open('document.csv', 'a', encoding="utf-8") as f:
 		f.write(string_to_write)
 
-#driver.close()
+	
+######################################################################################
+# MAIN
+######################################################################################
+open_browser()
+search_map()
+scrape_new_business()
+
+for i in range(5):
+	scrape_new_business()
+
+
+main()
+
+quit()
+
+'''
+search_text = sys.argv[1]
+search_text = search_text.replace(' ', '+')
+driver.get(f'https://www.google.com/maps/search/{search_text}')
+'''
+
